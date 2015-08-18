23e6dafd34e5e0468f7fc4de476e8278a37eaddd
AL10R14-2 <edgar_samir@hotmail.com>
2015-08-18 11:38:47 -0500

    ejercicio04
    boton de audio

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,25 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
-}); 
+//document.addEventListener("deviceready",function(){
+	 $('#btn_sonido').click(function(){
+//   alert("click btn_sonido");
+   if($('#btn_sonido').hasClass('ui-icon-audio'))
+   {
+//apagar audio
+     $('#btn_sonido').removeClass('ui-icon-audio');
+	 $('#btn_sonido').addClass('ui-icon-bars');
+	 alert('AUDIO APAGADO')
+	 
+	 }
+	 else
+	 {
+//encender audio
+	 $('#btn_sonido').removeClass('ui-icon-bars');
+	 $('#btn_sonido').addClass('ui-icon-audio');
+	 navigator.notification.beep(1);
+	 alert('AUDIO ENCENDIDO');
+	 }
+	 //alert("click a btn_sonido")
+  });//click a btn_sonido
+//});
 });
-
